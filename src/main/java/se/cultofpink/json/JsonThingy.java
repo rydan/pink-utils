969106c3d1c3ecba287d486ca1c01fdc0668c556
rydan <patrik@cultofpink.se>
2022-10-04 11:13:44 +0200

    More manipulation, upp:ed java version to 17

--- src/main/java/se/cultofpink/json/JsonThingy.java
+++ src/main/java/se/cultofpink/json/JsonThingy.java
@@ -1,11 +1,13 @@
 package se.cultofpink.json;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
-import com.fasterxml.jackson.databind.util.RawValue;
 
-import java.io.IOException;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.function.Supplier;
 
 /** JsonThingy is a container for working with JsonStyle objects
  *  in a handy way, without having to convert to/from POJO:s
@@ -16,9 +18,15 @@ public class JsonThingy {
     private final JsonNode root;
     private final ObjectMapper mapper;
 
-    public JsonThingy(String jsonString) throws IOException {
+    public JsonThingy(String jsonString) {
+        JsonNode root1;
         mapper = new ObjectMapper();
-        root = mapper.readTree(jsonString);
+        try {
+            root1 = mapper.readTree(jsonString);
+        } catch (JsonProcessingException e) {
+            root1 = mapper.nullNode();
+        }
+        root = root1;
     }
 
     private JsonThingy(JsonNode treeNode) {
@@ -28,12 +36,19 @@ public class JsonThingy {
 
     /** get in object... */
     public JsonThingy get(String key) {
-        return new JsonThingy(root.get(key));
+        if (!root.isObject()) {
+            throw new UnsupportedOperationException("Can't get value for key on non-object: " + this.get());
+        }
+        JsonNode treeNode = root.get(key);
+        return thingieFromNode(treeNode);
     }
 
     /** get element in array */
     public JsonThingy get(int index) {
-        return new JsonThingy(root.get(index));
+        if (!root.isArray()) {
+            throw new UnsupportedOperationException("Can't get position on non-array: " + this.get());
+        }
+        return thingieFromNode(root.get(index));
     }
 
     /**return a new JsonThingy-object containing the current JsonThingie at the specified key */
@@ -42,15 +57,49 @@ public class JsonThingy {
         return new JsonThingy(objectNode.set(key, this.root));
     }
 
-    public JsonThingy set(String key, JsonThingy value) throws IOException {
-        return new JsonThingy(((ObjectNode)(new JsonThingy(this.get()).root)).set(key, value.root));
+    public JsonThingy set(String key, JsonThingy value) {
+        if (!root.isObject()) {
+            throw new UnsupportedOperationException("Can't set on non-object: " + this.get());
+        }
+        ((ObjectNode) this.root).set(key, value.root);
+        return this;
     }
 
     /** get the string value currently navigated to. Could be simple string, JSON object or JSON array */
     public String get() {
         if (root.isArray() || root.isObject()) {
             return root.toString();
+        } else if (root.isNull()) {
+            return null;
         }
         return root.asText();
     }
+
+    public String orElse(String s) {
+        return Objects.requireNonNullElse(this.get(), s);
+    }
+
+    public static Optional<JsonThingy> safe(Supplier<JsonThingy> f) {
+        try {
+            return Optional.of(f.get());
+        } catch (UnsupportedOperationException e) {
+            return Optional.empty();
+        }
+    }
+
+    public static String safeGet(Supplier<JsonThingy> f) {
+        try {
+            return f.get().get();
+        } catch (UnsupportedOperationException e) {
+            return null;
+        }
+    }
+
+    private JsonThingy thingieFromNode(JsonNode treeNode) {
+        if (null != treeNode) {
+            return new JsonThingy(treeNode);
+        }
+        return new JsonThingy(mapper.nullNode());
+    }
+
 }
--- src/test/java/se/cultofpink/json/JsonThingyTest.java
+++ src/test/java/se/cultofpink/json/JsonThingyTest.java
@@ -2,8 +2,6 @@ package se.cultofpink.json;
 
 import org.junit.jupiter.api.Test;
 
-import java.io.IOException;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class JsonThingyTest {
@@ -11,7 +9,8 @@ class JsonThingyTest {
     private static final String jsonWithArray = "{\"hej\":\"hejsvej\",\"array\":[\"first\",\"second\",3]}";
 
     @Test
-    public void conceptDeclaration() throws IOException {
+    public void conceptDeclaration() {
+        assertEquals("hej", new JsonThingy("\"hej\"").get());
         assertEquals("hejhej", new JsonThingy(simpleJson).get("hej").get());
         assertEquals("hej igen", new JsonThingy(simpleJson).get("obj").get("inner").get());
         assertEquals("{\"inner\":\"hej igen\"}", new JsonThingy(simpleJson).get("obj").get());
@@ -20,8 +19,43 @@ class JsonThingyTest {
         assertEquals("[\"first\",\"second\",3]", new JsonThingy(jsonWithArray).get("array").get());
         assertEquals("3", new JsonThingy(jsonWithArray).get("array").get(2).get());
 
+        assertNull(new JsonThingy(simpleJson).get("nonexistent").get());
+        try {
+            assertNull(new JsonThingy(simpleJson).get("nonexistent").get("nonexistent").get());
+            fail("Should have thrown...");
+        } catch (UnsupportedOperationException e) {
+            //OK, expected
+        }
+
+
         assertEquals(simpleJson, new JsonThingy(simpleJson).contain("contained").get("contained").get());
         assertEquals("{\"test\":\"fjuff\",\"add\":" + jsonWithArray + "}",
                 new JsonThingy("{\"test\":\"fjuff\"}").set("add", new JsonThingy(jsonWithArray)).get());
+        try {
+            new JsonThingy("[]").set("hepp", new JsonThingy(simpleJson));
+            fail("Should have thrown...");
+        } catch (UnsupportedOperationException e) {
+            //OK!
+        }
+    }
+
+    @Test
+    public void testSafeGet() {
+        assertEquals("hejsvej", JsonThingy.safeGet(() -> new JsonThingy(jsonWithArray).get("hej")));
+        assertEquals("second", JsonThingy.safeGet(() -> new JsonThingy(jsonWithArray).get("array").get(1)));
+        assertEquals("[\"first\",\"second\",3]", JsonThingy.safeGet(() -> new JsonThingy(jsonWithArray).get("array")));
+        assertEquals("3", JsonThingy.safeGet(() -> new JsonThingy(jsonWithArray).get("array").get(2)));
+
+        assertNull(JsonThingy.safeGet(() -> new JsonThingy(simpleJson).get("nonexistent")));
+        assertNull(JsonThingy.safeGet(() -> new JsonThingy(simpleJson).get("nonexistent").get("nonexistent").get("nonexistent").get("nonexistent")));
+    }
+
+    @Test
+    public void testOrElse() {
+        assertEquals("[\"first\",\"second\",3]", new JsonThingy(jsonWithArray).get("array").orElse("Hej!"));
+        assertEquals("3", new JsonThingy(jsonWithArray).get("array").get(2).orElse("Hej!"));
+        assertEquals("Hej!", new JsonThingy(simpleJson).get("nonexistent").orElse("Hej!"));
+
+
     }
 }
